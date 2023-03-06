cbae056277bbb53be4d4d8a6d67e4da2717794f2
JanaFTouni <104241980+JanaFTouni@users.noreply.github.com>
2023-03-06 15:37:08 -0300

    Add files via upload

--- dates.py
+++ dates.py
@@ -1,15 +1,26 @@
+#Table de Hachage
+#Un tableau de 23 tableaux
+
 def cree():
     """Cree et renvoie un ensemble de dates vide. """
-    return [0] * 6
+    return [[] for _ in range(23)]
 
 def contient(ens, val) -> bool:
     """Renvoie True si ens contient val."""
-    paquet = val // 64
-    bit = val % 64
-    return (ens[paquet] & (1 << bit)) != 0
+    return val in ens[val % 23]
 
 def ajoute(ens, val) -> None:
     """Ajoute val a ens"""
-    paquet = val // 64
-    bit = val % 64
-    ens[paquet] = (ens[paquet] | (1 << bit))
+    ens[val % 23].append(val)
+    
+def enumere(ens) -> list:
+    """Renvoie un tableau contenant les elements de lensemble ens."""
+    tab = []
+    for paquet in ens:
+        for elt in paquet:
+            tab.append(elt)
+    return tab
+
+#Tests
+e0 = [[46, 92],[1,47],[],[],[], [],[],[],[],[],[],[],[],[],[],[22],[],[],[],[],[],[],[]]
+assert enumere(e0) == [46,92,1,47,22]
