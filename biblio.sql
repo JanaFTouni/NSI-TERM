24b7bbd65d8e8698934689d881be03ba910e2c2e
JanaFTouni <104241980+JanaFTouni@users.noreply.github.com>
2023-03-08 09:48:08 -0300

    Add files via upload

---
+++ biblio.sql
@@ -0,0 +1,34 @@
+CREATE TABLE usager ( 
+	nom VARCHAR(90),
+	prenom VARCHAR(90),
+	adresse VARCHAR(300),
+	cp VARCHAR(5), 
+	ville VARCHAR(60),
+	email VARCHAR(60),
+	code_barre CHAR(15) PRIMARY KEY
+	);
+	
+CREATE TABLE livre(
+	titre VARCHAR(300),
+	editeur VARCHAR(90),
+	annee INT,
+	isbn CHAR(14) PRIMARY KEY
+	);
+
+CREATE TABLE auteur(
+	nom VARCHAR(300),
+	prenom VARCHAR(90),
+	a_id INT PRIMARY KEY
+	);
+	
+CREATE TABLE auteur_de(
+	a_id INT REFERENCES auteur(a_id),
+		isbn CHAR(14) REFERENCES livre(isbn),
+		PRIMARY KEY (a_id, isbn)
+		);
+	
+CREATE TABLE emprunt(
+	code_barre CHAR(15) REFERENCES usager(code_barre),
+	isbn CHAR(15) PRIMARY KEY REFERENCES livre(isbn),
+	retour DATE
+
