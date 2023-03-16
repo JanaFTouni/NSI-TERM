dc082a7b9468bae88ea8cb2b38cfd2d048c1d68d
JanaFTouni <104241980+JanaFTouni@users.noreply.github.com>
2023-03-16 09:48:51 -0300

    Add files via upload

---
+++ Chrono continuation.py
@@ -0,0 +1,21 @@
+
+class Chrono:
+    """Une classe pour representer un temps mesure en heures, minutes, secondes."""
+    heure_max = 24 #Attribut de classe
+    #initialiseur
+    def __init__(self, h, m, s):
+        self.heures = h #attributs d'instance
+        self.minutes = m
+        self.secondes = s
+        
+    def texte(self):
+        return f"{self.heures}h {self.minutes}min {self.secondes}sec"
+    
+    def avance(self,s):
+        self.secondes += s
+        # depassement secondes
+        self.minutes += self.secondes // 60
+        self.secondes = self.secondes % 60
+        #depassement minutes
+        self.heures += self.minutes // 60
+        self.minutes = self.minutes & 60
