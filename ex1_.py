b9ee6268744529d1e9c37dbf3cf3371ad1d0b56a
JanaFTouni <104241980+JanaFTouni@users.noreply.github.com>
2023-04-12 09:00:54 -0300

    Add files via upload

--- ex1_.py
+++ ex1_.py
@@ -12,7 +12,50 @@ from nieme_element  import nieme_element
 def listeN(n: int) -> Cellule:
     lst = None
     cpt = n
-    for _ in tange(n,0, -1):
+    for _ in range(n,0, -1):
         lst = Celulle(cpt, lst)
     return lst
+
+def affiche_liste(lst : Cellule) -> None:
+    if lst is None:
+        print()
+    else:
+        print(lst.valeur, end = " ")
+        affiche_liste(lst.suivante)
+        
+l1 = Cellule(5,Cellule(2, Cellule(7, Cellule(4, None))))
+
+
+def affiche_liste(lst: Cellule) -> None:
+    courant = lst
+    while courant is not None:
+        print(courant.valeur, end = " ")
+        courant = courant.suivante
+    print()
     
+
+def nieme_element(n: int, lst: Cellule):
+    """Renvoie le n-ieme element de la liste lst
+    les elements sont numerotes a partir de 0"""
+    if n <0 or lst is None:
+        raise IndexError("Indice invalide")
+    if n == 0:
+        return lst.valeur
+    else :
+        return nieme_element(n - 1, lst.suivante)
+    
+def nieme_element(n: int, lst: Cellule) -> None:
+    courant = lst
+    a = 0
+    if n < 0 :
+        raise IndexError("Indice invalide")
+    while a < n and courant is not None:
+        a += 1
+        courant = courant.suivante
+    if courant is None :
+        raise IndexError("Indice invalide")
+    return courant.valeur
+        
+l2 = Cellule(3, Cellule(5, Cellule(1, None)))
+assert nieme_element(0, l2) == 3
+        
